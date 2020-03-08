d9cc95d46eefb980d63374f06d5d2776691345b6
Lch0609 <1346711267@qq.com>
2020-03-09 00:24:43 +0800

    diyici

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @Classname PaymentMain8001
+ * @Description TODO
+ * @Date 2020/3/8 23:18
+ * @Created by 李传豪
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
